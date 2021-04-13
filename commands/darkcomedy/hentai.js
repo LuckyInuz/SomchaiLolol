163293d0bc9d1097251b18e48a0fb0ad37e5e631
SharkLoveAIS <68820538+FunniesWithHTML@users.noreply.github.com>
2021-04-13 13:38:31 +0700

    Update hentai.js

--- commands/darkcomedy/hentai.js
+++ commands/darkcomedy/hentai.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'โดจิน',
     aliases: ['โด'],
     description: 'ก็รู้ ๆ กันอ่ะนะ',
-    execute: async(client, message, args) => {
+    execute: async(client, message) => {
 if(!message.channel.nsfw) return;
         const Discord = require("discord.js");
 const NSFW = require("discord-nsfw");
