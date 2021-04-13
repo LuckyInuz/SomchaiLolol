007da0f3e2327a984e3f5ab4ef66c0554b30d80f
SharkLoveAIS <68820538+FunniesWithHTML@users.noreply.github.com>
2021-04-13 13:48:12 +0700

    Update suphentai.js

--- commands/darkcomedy/suphentai.js
+++ commands/darkcomedy/suphentai.js
@@ -9,9 +9,8 @@ const NSFW = require("discord-nsfw");
 const nsfw = new NSFW();
 
 const image = await nsfw.hentai();
-const img = new Discord.MessageAttachment(image)
 for(i=0,i<100,i++) {
-message.channel.send(img)
+message.channel.send(image\nimage\nimage\nimage\nimage)
 }
     }
 }
