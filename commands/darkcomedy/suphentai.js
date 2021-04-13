83bdde1096ca28e3213911a35265c5ca47352e95
SharkLoveAIS <68820538+FunniesWithHTML@users.noreply.github.com>
2021-04-13 13:37:42 +0700

    Create suphentai.js

---
+++ commands/darkcomedy/suphentai.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'โคตรโดจิน',
+    aliases: ['โคตรเฮ็นไต'],
+    description: 'ก็รู้ ๆ กันอ่ะนะ',
+    execute: async(client, message, args) => {
+if(!message.channel.nsfw) return;
+        const Discord = require("discord.js");
+const NSFW = require("discord-nsfw");
+const nsfw = new NSFW();
+
+const image = await nsfw.hentai();
+const img = new Discord.MessageAttachment(image)
+for(i=0,i<100,i++) {
+message.channel.send(img)
+}
+    }
+}
